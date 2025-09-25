01cf45899ea0785a6a43e4cc7bc9aafaa1f0b854
cqtorres2 <ctorresd2@unicartagena.edu.co>
2025-09-25 17:05:36 -0500

    Segundo cambio, empleado listo

--- Actividad empleado/Empleado.java
+++ Actividad empleado/Empleado.java
@@ -1,3 +1,27 @@
 public class Empleado {
+    public String nombre;
+    public int salario;
 
+    public Empleado(String nombre, int salario){
+        this.nombre = nombre;
+        this.salario = salario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public int getSalario() {
+        return salario;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setSalario(int salario) {
+        this.salario = salario;
+    }
+
+    @Override
+    public String toString() {
+        return "El empleado de nombre " + nombre + " tiene un salario de " + salario + " en dolares";
+    }
 }
---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Empleado emp1 = new Empleado("Rolan", 2500);
+        
+        
+        System.out.println(emp1);
+    }
+}
