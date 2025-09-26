a32395b92f5318d8eff57013eb1106393bcabd92
cqtorres2 <ctorresd2@unicartagena.edu.co>
2025-09-26 11:32:01 -0500

    Ultimo utilidades

--- Actividad Utilidades/Utilidades.java
+++ Actividad Utilidades/Utilidades.java
@@ -1,6 +1,6 @@
 public class Utilidades {
-    public int Numero1;
-    public int Numero2;
+    public int Numero1 = 23;
+    public int Numero2 = 45;
     public int Resultado;
 
     public int suma(int Numero1, int Numero2, int Resultado) {
@@ -20,14 +20,19 @@ public class Utilidades {
         this.Numero2 = Numero2;
         return Resultado = Numero1 * Numero2;
     }
-        public double Dividir (int Numero1, int Numero2, int Resultado){
+
+    public double Dividir(int Numero1, int Numero2, int Resultado) {
         this.Numero1 = Numero1;
         this.Numero2 = Numero2;
-        return Resultado = Numero1 * Numero2;
+        return Resultado = Numero1 / Numero2;
     }
 
     @Override
     public String toString() {
-        return "El ejercicio propuesto consta de los numeros " + Numero1 + " y " + Numero2 + " donde la suma de ambos es " + suma(Numero1, Numero2, Resultado) + " su resta consta de  " + resta(Numero1, Numero2, Resultado) + " con una multiplicación de " + Multiplicar(Numero1, Numero2, Resultado) + " Que es dividido con un resultado de " + Dividir(Numero1, Numero2, Resultado);
+        return "El ejercicio propuesto consta de los numeros " + Numero1 + " y " + Numero2
+                + " donde la suma de ambos es " + suma(Numero1, Numero2, Resultado) + " su resta consta de  "
+                + resta(Numero1, Numero2, Resultado) + " con una multiplicación de "
+                + Multiplicar(Numero1, Numero2, Resultado) + " Que es dividido con un resultado de "
+                + Dividir(Numero1, Numero2, Resultado);
     }
 }
--- Main.java
+++ Main.java
@@ -2,8 +2,11 @@ public class Main {
     public static void main(String[] args) {
         Empleado emp1 = new Empleado("Rolan", 2500);
         CuentaBancaria cuen1 = new CuentaBancaria(13256, 1500, "Juridica");
+        Utilidades uti1 = new Utilidades();
         
         System.out.println(cuen1);
         System.out.println(emp1);
+        System.out.println(uti1);
+
     }
 }
